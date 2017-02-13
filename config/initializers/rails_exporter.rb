42db46a0cec5171720b9ec3bb3399015f923573f
Bruno Porto <brunotporto@gmail.com>
2017-02-13 12:55:32 -0200

    atualizando versao

--- config/initializers/rails_exporter.rb
+++ config/initializers/rails_exporter.rb
@@ -1,6 +1,5 @@
 class Railtie < ::Rails::Railtie
   initializer 'activeservice.autoload', :before => :set_autoload_paths do |app|
-    raise app.inspect
     app.config.autoload_paths += %( #{Rails.application.config.root}/app/exporters )
   end
 end
